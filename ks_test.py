a6d7436e9b9646f6d5b10d42dbc46c50ff0ca631
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-10-23 17:31:38 -0400

    Testing for Solution Implementations
    Add tests and testing methodology to confirm that solutions are indeed correct.

---
+++ ks_test.py
@@ -0,0 +1,36 @@
+from knapsack import *
+
+
+def test1(solver: callable):
+    choices: list[tuple[int, int]] = [(5, 6), (6, 5), (4, 6), (6, 6), (5, 3), (2, 7)]
+    cap: int = 15
+    assert solver(choices, cap) == 17
+    print('PASS 1')
+
+
+def test2(solver: callable):
+    choices: list[tuple[int, int]] = [(10 ** 9, 1) for _ in range(5)]
+    cap: int = 5
+    assert solver(choices, cap) == (5 * 10 ** 9)
+    print('PASS 2')
+
+
+def test3(solver: callable):
+    choices: list[tuple[int, int]] = [(30, 3), (50, 4), (60, 5)]
+    cap: int = 8
+    assert solver(choices, cap) == 90
+    print('PASS 3')
+
+
+def main():
+    solvers = [ks_bottom_up]
+    for solver in solvers:
+        print('*********', solver.__name__, '*********')
+        test1(solver)
+        test2(solver)
+        test3(solver)
+        print('#########', solver.__name__, '#########')
+
+
+if __name__ == '__main__':
+    main()
