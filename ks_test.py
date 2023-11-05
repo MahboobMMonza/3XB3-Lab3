b31167d6180e21e0e961e1e5a06f6be55e4923ec
Kfmoi <faragk1@mcmaster.ca>
2023-11-05 13:35:39 -0500

    Added thee top_down solver to test, Passed all tests

--- ks_test.py
+++ ks_test.py
@@ -23,7 +23,7 @@ def test3(solver: callable):
 
 
 def main():
-    solvers = [ks_bottom_up, ks_brute_force]
+    solvers = [ks_bottom_up, ks_brute_force, ks_top_down]
     for solver in solvers:
         print('*********', solver.__name__, '*********')
         test1(solver)
