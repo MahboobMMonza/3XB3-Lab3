84055ab9f13fcb033809c8b047613d55aea3f445
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-11-05 19:39:47 -0500

    Add testing for `ks_recursive`
    Add `ks_recursive` to the list of tested solvers.

--- ks_test.py
+++ ks_test.py
@@ -23,7 +23,7 @@ def test3(solver: callable):
 
 
 def main():
-    solvers = [ks_bottom_up, ks_brute_force, ks_top_down]
+    solvers = [ks_bottom_up, ks_top_down, ks_brute_force, ks_recursive]
     for solver in solvers:
         print('*********', solver.__name__, '*********')
         test1(solver)
