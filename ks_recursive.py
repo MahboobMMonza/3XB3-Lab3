e442d9d1d7a0b8ea9e3098505c24a861210e088e
Samih Dalvi <dalvis3@mcmaster.ca>
2023-11-05 19:20:30 -0500

    Modified comments

--- ks_recursive.py
+++ ks_recursive.py
@@ -3,14 +3,14 @@ def ks_recursive(items: list[tuple[int, int]], capacity: int) -> int:
     if not items or capacity == 0:
         return 0
 
-    # Start with the last item in the list (index i)
+    # Start with the last item in the list index i
     i = len(items)
 
-    # If the weight of the last item exceeds the current capacity, skip it.
+    # If the weight of the last item exceeds the current capacity don't include it
     if items[i - 1][0] > capacity:
         return ks_recursive(items[:-1], capacity)
     else:
-        # Calculate the maximum value by either taking or leaving the last item.
+        # Calculate the maximum value by either taking or leaving the last item
         take_item = ks_recursive(items[:-1], capacity - items[i - 1][0]) + items[i - 1][1]
         leave_item = ks_recursive(items[:-1], capacity)
         return max(take_item, leave_item)
