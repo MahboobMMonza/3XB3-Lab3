0064e916a77a98d025586009ed8682e843349f5a
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-11-13 15:48:40 -0500

    Correct Comments for Brick Lab Implementation
    Correct the explanation and add optimization for binary search case.

--- brick_lab.py
+++ brick_lab.py
@@ -7,30 +7,29 @@ Optimal Substructure
 Note: This problem is the same as finding some level equivalent to k* - 1 within levels 0...n - 1 (where level 0
 signifies there is no level that won't break the brick). This is because k* is the first level at which the brick will
 break, and so k* - 1 is the last level for which it won't break. Since a brick is guaranteed to break at some level
-between 1 and n, it is guaranteed to break at n, so we only need to consider levels 0 to n - 1.
+between 1 and n, it is guaranteed to break at n, so we only need to consider levels 0 to n - 1 for non-breaking levels.
 
 Case 1:
-If m >= floor(log_2(n - 1)) + 1, then we can binary search without issues. I will need floor(log_2(n - 1)) + 1 runs on
-the machine to know what k* is (as stated in the lab instructions). Note that log_2(0) = 0 for our purposes, and integer
-division is assumed. The only exception would be that no testing would be required if there is only 1 level.
+If m >= floor(log_2(n - 1)) + 1, then it is possible to binary search without issues. floor(log_2(n - 1)) + 1 runs on
+the machine are required to know what k* is (as stated in the lab instructions). Note that log_2(0) = 0 in this
+calculation. The only exception would be that no testing would be required if there is only 1 level.
 
 Case 2:
 If m = 1, then our only option is to go up one-by-one until it doesn't break. The last non-breaking value can be n - 1,
 so n - 1 tests are required at worst case.
 
 Case 3:
-When there are not enough bricks to do a binary search, we "try" every level and see which one gave the lowest
-worst-case. At each level w, if the brick breaks, we would need to check w - 1 levels with one less brick. If the brick
-doesn't break, then we need to check N - w levels with the same number of bricks. Of these two options, we choose the
-one with the highest total as that is the worst-case and add 1 to it (to represent the force test at level w). Then,
-among all N levels, we choose the one with the lowest worst-case if the first brick was tested at that level first to
-determine the lowest number of tests necessary to find k* - 1, which is the same as the number of bricks needed to find
-k*.
+When there are not enough bricks to do a binary search, "try" every level and see which one gives the lowest worst-case.
+At each level w, if the brick breaks, check w - 1 levels with one less brick. If the brick doesn't break, then check
+i - w levels with the same number of bricks. Of these two options, choose the one with the highest total as that is the
+worst-case and add 1 to it (to represent the force test at level w). Then, among all i levels, choose the one with the
+lowest worst-case if the first brick was tested at that level to determine the lowest number of tests necessary to find
+k* - 1, which in turn is the same as the number of bricks needed to find k*.
 
 ========================================================================================================================
 Recurrence Relation
 
-Test(i, j) => worst-case tests needed with j bricks and i levels. The levels here are from 0 to N (= n - 1), NOT 1 to n
+Test(i, j) => worst-case tests needed with j bricks and i levels. The levels here are from 0 to N = n - 1 [NOT 1 to n]
 
 Base Case 1:
 With i levels and 1 brick, i levels have to be tested one-by-one, so Test(i, 1) = i.
@@ -44,17 +43,18 @@ With no bricks, no levels can be tested. This won't mess up calculations because
 maximum. Test(i, 0) = 0.
 
 Otherwise, the rules for Case 3 apply, and for i levels and j bricks:
-Test(i, j) = min(1 + max(Test(N - k, j), Test(k - 1, j - 1))) for all k in [1, i]
+Test(i, j) = min(1 + max(Test(i - k, j), Test(k - 1, j - 1))) for all k in [1, i]
 
 There will be many cases of overlapping sub-problems that would require recalculation if not cached, so top-down will be
-an easy way to tackle the problem, as cases will be solved when they are first encountered.
+an easy way to tackle the problem, as cases will be solved when they are first encountered. Top-down facilitates a more
+intuitive implementation of the solution.
 
 ########################################################################################################################
 Test(i, j):
 * Test(i, 1) = i
 * Test(1 >= i >= 0, j >= 1) = i
 * Test(i, 0) = 0
-* Test(i, j) = min(1 + max(Test(N - k, j), Test(k - 1, j - 1))) for all k in [1, i]
+* Test(i, j) = min(1 + max(Test(i - k, j), Test(k - 1, j - 1))) for all k in [1, i]
 ########################################################################################################################
 """
 
@@ -100,8 +100,13 @@ def test_levels(n: int, m: int) -> (list[list[int]], list[list[int]]):
         tests[j][0] = 0
         next_level[j][0] = 0
 
-    # Make recursive call to helper and get result
-    test_levels_helper(n - 1, m, tests, next_level)
+    # Take care of the binary search case
+    if (n - 1).bit_length() <= m:
+        tests[m][n - 1] = (n - 1).bit_length()
+        next_level[m][n - 1] = n // 2
+    else:
+        # Otherwise, make the recursive call to helper and get result
+        test_levels_helper(n - 1, m, tests, next_level)
     return tests, next_level
 
 
